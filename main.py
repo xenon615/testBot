d0d9c36e5caa200e33f3da85d34241d7c51e0476
xenon615 <xenon615@gmail.com>
2023-03-16 19:59:38 +0200

    init

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-
 import telebot
-import json
 token = '6148577713:AAEMqW4wBgGw57wTYBMkJkFnpc8OIBjD-_g'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message: telebot.types.Message):
     bot.send_message(message.from_user.id, 'echo')
-
-
-
 bot.infinity_polling()
