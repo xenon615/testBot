d4147431bc43221cc0fd42a18a9da51d1e1cb133
xenon615 <xenon615@gmail.com>
2023-03-18 17:11:40 +0200

    start2

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import socket
 import os
 
 import telebot
 # token = '6148577713:AAEMqW4wBgGw57wTYBMkJkFnpc8OIBjD-_g'
 token = os.environ['TOKEN']
 bot = telebot.TeleBot(token)
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind(('localhost', 666))
+
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message: telebot.types.Message):
