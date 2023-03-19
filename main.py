a3fc1e779ef6ddc0c1af49c711302a67f18cd981
xenon615 <xenon615@gmail.com>
2023-03-19 15:27:34 +0200

    start4

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import telebot
 # token = '6148577713:AAEMqW4wBgGw57wTYBMkJkFnpc8OIBjD-_g'
 token = os.environ['TOKEN']
 bot = telebot.TeleBot(token)
-
+hostname = socket.gethostname()
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message: telebot.types.Message):
-    bot.send_message(message.from_user.id, 'Я работаю автономно , я сбежал !!!!')
+    bot.send_message(message.from_user.id, 'Я работаю автономно на ' + hostname)
 bot.infinity_polling()
